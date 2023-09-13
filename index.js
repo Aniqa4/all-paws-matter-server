9cf9a090fa05d1de118c5461c72f06143fc6d3f2
Aniqa <aanika.chowdhury04@gmail.com>
2023-09-13 08:01:20 +0600

    connected to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,11 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//schema
+const servicesSchema= new mongoose.Schema(
+    {
+        service_name:{
+            type: String
+        },
+        description:{
+            type:String
+        }
+    }
+)
+
+const Services=mongoose.model('services',servicesSchema)
+
+//connecting to mongodb
+mongoose
+    .connect(`mongodb+srv://${process.env.user_name}:${process.env.password}@cluster0.3kab2ba.mongodb.net/?retryWrites=true&w=majority`)
+    .then(()=>console.log('MongoDB Connected'))
+    .catch((err)=>console.log("Mongo Error",err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Server is running at port ${port}`)
+    console.log(`Server is running at port ${port}`)
 })
