8d9fbd4e24ebc7c23a1f0bc407eaa9215fbeec6c
Aniqa <aanika.chowdhury04@gmail.com>
2023-09-13 08:11:22 +0600

    get data from database

--- index.js
+++ index.js
@@ -25,9 +25,25 @@ const Services=mongoose.model('services',servicesSchema)
 
 //connecting to mongodb
 mongoose
-    .connect(`mongodb+srv://${process.env.user_name}:${process.env.password}@cluster0.3kab2ba.mongodb.net/?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://${process.env.user_name}:${process.env.password}@cluster0.3kab2ba.mongodb.net/services?retryWrites=true&w=majority`)
     .then(()=>console.log('MongoDB Connected'))
     .catch((err)=>console.log("Mongo Error",err))
+
+//get all data
+app.get('/services', async (req, res) => {
+    try {
+      // Use the Services model to fetch data from the "services" collection
+      const services = await Services.find();
+  
+      // Send the retrieved data as a JSON response
+      res.json(services);
+    } catch (error) {
+      // Handle any errors that occur during the database query
+      console.error('Error fetching services:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });  
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
